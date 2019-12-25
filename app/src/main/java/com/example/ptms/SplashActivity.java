d2138c3af69ea4f15f6f1cfb86c6c3e2a4320933
Devin1996 <devinchandula@gmail.com>
2019-12-25 10:30:21 +0530

    Firebase Connection Established SplashScreen Created briefly Working on WelcomeScreen

---
+++ app/src/main/java/com/example/ptms/SplashActivity.java
@@ -0,0 +1,40 @@
+package com.example.ptms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread thread = new Thread()
+        {
+            @Override
+            public void run(){
+                try{
+                    sleep(7000);
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }finally{
+                    Intent WelcomeIntent = new Intent(SplashActivity.this, WelcomeActivity.class);
+                    startActivity(WelcomeIntent);
+                }
+            }
+
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+
+}
