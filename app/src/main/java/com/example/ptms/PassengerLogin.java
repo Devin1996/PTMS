9dba5869307e7753be499c6ab44088fd0c33316f
Devin1996 <devinchandula@gmail.com>
2019-12-25 15:01:25 +0530

    Changes to be committed: 	new file:   app/src/main/java/com/example/ptms/ConductorLogin.java 	new file:   app/src/main/java/com/example/ptms/DriverLogin.java 	new file:   app/src/main/java/com/example/ptms/PassengerLogin.java 	new file:   app/src/main/java/com/example/ptms/TestActivity.java

---
+++ app/src/main/java/com/example/ptms/PassengerLogin.java
@@ -0,0 +1,14 @@
+package com.example.ptms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PassengerLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passenger_login);
+    }
+}
--- app/src/main/java/com/example/ptms/WelcomeActivity.java
+++ app/src/main/java/com/example/ptms/WelcomeActivity.java
@@ -2,13 +2,49 @@ package com.example.ptms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    private Button WelcomeDriverBtn;
+    private Button WelcomePassengerBtn;
+    private Button WelcomeConductorBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        WelcomeDriverBtn = (Button) findViewById(R.id.wel_dri_btn);
+        WelcomePassengerBtn = (Button) findViewById(R.id.wel_pas_btn);
+        WelcomeConductorBtn = (Button) findViewById(R.id.wel_con_btn);
+
+        WelcomeDriverBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeActivity.this, PassengerLogin.class);
+                startActivity(intent);
+            }
+        });
+
+        WelcomeDriverBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeActivity.this, DriverLogin.class);
+                startActivity(intent);
+            }
+        });
+
+        WelcomeDriverBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeActivity.this, ConductorLogin.class);
+                startActivity(intent);
+            }
+        });
     }
-}
+    }
