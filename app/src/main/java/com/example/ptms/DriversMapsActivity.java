4db03ddf31b6098b4d70d0f1f0f2389f682777a8
Devin1996 <devinchandula@gmail.com>
2019-12-27 19:53:05 +0530

    Get the Current location of the usr
    Changes to be committed:
    	modified:   app/src/main/AndroidManifest.xml
    	modified:   app/src/main/java/com/example/ptms/DriversMapsActivity.java

--- app/src/main/java/com/example/ptms/DriversMapsActivity.java
+++ app/src/main/java/com/example/ptms/DriversMapsActivity.java
@@ -1,9 +1,20 @@
 package com.example.ptms;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.location.Location;
 import android.os.Bundle;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -11,9 +22,15 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class DriversMapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class DriversMapsActivity extends FragmentActivity implements OnMapReadyCallback,
+                                                                        GoogleApiClient.ConnectionCallbacks,
+                                                                        GoogleApiClient.OnConnectionFailedListener,
+                                                                        com.google.android.gms.location.LocationListener{
 
     private GoogleMap mMap;
+    GoogleApiClient googleApiClient;
+    Location lastLocation;
+    LocationRequest locationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +56,63 @@ public class DriversMapsActivity extends FragmentActivity implements OnMapReadyC
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        buildGoogleAPIClient();
+
+        //locate the usr location
+        mMap.setMyLocationEnabled(true);
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        locationRequest = new LocationRequest();
+
+        //location Will be refreshed at every 10s
+        locationRequest.setInterval(1000);
+        locationRequest.setFastestInterval(1000);
+        
+        locationRequest.setPriority(locationRequest.PRIORITY_HIGH_ACCURACY);
+
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+//            return;
+//        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        //Get the current location
+        lastLocation = location;
+
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(12));
+    }
+
+    protected synchronized void buildGoogleAPIClient(){
+        googleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        googleApiClient.connect();
     }
 }
