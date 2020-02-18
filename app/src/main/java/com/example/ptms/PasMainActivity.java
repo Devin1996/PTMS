714c40d2a23049aa0e0bdfaefbaa99914b7564c6
Devin1996 <devinchandula@gmail.com>
2020-02-18 10:30:41 +0530

    Passenger Login and Register Front End Done

--- app/src/main/java/com/example/ptms/PasMainActivity.java
+++ app/src/main/java/com/example/ptms/PasMainActivity.java
@@ -2,13 +2,38 @@ package com.example.ptms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PasMainActivity extends AppCompatActivity {
 
+    private Button joinNowButton, loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pas_main);
+
+        joinNowButton = (Button) findViewById(R.id.main_join_now_btn);
+        loginButton =(Button) findViewById(R.id.main_login_btn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PasMainActivity.this, PasLoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        joinNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PasMainActivity.this, PasRegActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
